3328e2d31d1147cbd04a8043e03e652801257b57
javajigi <javajigi@gmail.com>
2017-01-05 16:17:51 +0900

    initial commit

---
+++ src/main/java/rock/MyRockApplication.java
@@ -0,0 +1,12 @@
+package rock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRockApplication.class, args);
+	}
+}
