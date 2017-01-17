1b86d3c3746b310f40994d5d11be740383d4a772
javajigi <javajigi@gmail.com>
2017-01-17 22:27:13 +0900

    move main method

--- src/test/java/rock/MyInitialzer.java
+++
@@ -1,9 +0,0 @@
-package rock;
-
-import org.springframework.boot.SpringApplication;
-
-public class MyInitialzer {
-	public static void main(String[] args) {
-		SpringApplication.run(MyRockApplication.class, args);
-	}
-}
